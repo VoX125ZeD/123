40b772a96f93686b0a77842ee5502ee61fc9ce95
VoX125ZeD <VoX125ZeD@DESKTOP-DQAE9IS>
2021-01-31 22:41:15 +0500

    console

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+
+            
+            while (true)
+            {
+                string str = "";
+                for (int i = 0; i < 6; i++)
+                {
+                    if (ran.Next(0, 2) == 0)
+                    {
+                        if (ran.Next(0, 2) == 0)
+                        {
+                            str += (char)ran.Next(65, 90);
+                        }
+                        else
+                        {
+                            str += (char)ran.Next(97, 122);
+                        }
+                    }
+                    else
+                    {
+                        str += (char)ran.Next(49, 57);
+                    }
+                }
+                Console.WriteLine(str);
+                Console.Read();
+            }
+        }
+    }
+}
